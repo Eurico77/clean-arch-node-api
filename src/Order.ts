9630ebcacadbace27c15e17ea0d8e021e87711ec
eurico77 <euricomagalhaes61@gmail.com>
2022-12-03 23:44:36 -0300

    (feat): should be validate a cpf valid and invalid

--- src/Order.ts
+++ src/Order.ts
@@ -1,13 +1,13 @@
+import Cpf from './Cpf';
+
 export default class Order {
-  constructor(private cpf: string) {
-    
+  cpf: Cpf;
+  
+  constructor(cpf: string) {
+    this.cpf = new Cpf(cpf);
   }
 
   getTotal() {
     return 0;
   }
 }
-
-
-
- 
